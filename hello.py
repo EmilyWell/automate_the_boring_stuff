af101306b8269b1629f180ea6031cfe3bedbdc31
EmilyWell <emilywellgit@gmail.com>
2021-09-11 11:21:32 -0500

    starting automate the boring stuff

---
+++ hello.py
@@ -0,0 +1,10 @@
+# this program says hello and asks for my name
+
+print('Hello World!')
+print('What is your name?') # asks for thier name
+myName = input()
+print('Nice to meet you, ' + myName)
+print(len(myName))
+print('What is your age?') # asks for thier age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
